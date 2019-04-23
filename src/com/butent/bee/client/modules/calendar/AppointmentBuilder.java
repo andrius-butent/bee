f976461fd56ea028f58d1c42deb59ab883e94a92
andrius-butent <sharnan.andriux@gmail.com>
2019-04-23 20:34:28 +0300

    TradeActService in Appointment form

--- src/com/butent/bee/client/modules/calendar/AppointmentBuilder.java
+++ src/com/butent/bee/client/modules/calendar/AppointmentBuilder.java
@@ -1435,7 +1435,7 @@ class AppointmentBuilder extends AppointmentForm implements SelectorEvent.Handle
           columns.add(ClassifierConstants.COL_DATE_FROM);
         }
 
-        if (!Objects.equals(oldTradeActService, newTradeActService) && DataUtils.isId(getActiveRowId())) {
+        if (!Objects.equals(oldTradeActService, newTradeActService)) {
           values.add(BeeUtils.toString(getActiveRowId()));
           columns.add(COL_APPOINTMENT);
         }
